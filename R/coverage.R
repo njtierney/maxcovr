0ddca6cf074d1258274e07b6cf2546f4509d23f4
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-11-15 19:28:32 +1000

    add utility function `coverage`
    to calculate the coverage of facilities and users

---
+++ R/coverage.R
@@ -0,0 +1,38 @@
+#' coverage
+#'
+#' @param df_dist distance matrix, as computed by facility_user_dist
+#' @param dist_indic the critical distance range that you would like to know,
+#'     default is 100m
+#' @param spread do you want this in a one row summary, or a two row summary?
+#'     Default is TRUE, a one row summary
+#'
+# @importFrom magrittr %>%
+#'
+#' @return dataframe
+#' @export
+#'
+coverage <- function(df_dist,
+                     dist_indic = 100,
+                     spread = TRUE){
+
+    if(spread == FALSE){
+
+        df_dist %>%
+            dplyr::count(is_covered = distance < dist_indic)  %>%
+            dplyr::mutate(is_covered = dplyr::if_else(is_covered == TRUE,
+                                                      true = "Covered",
+                                                      false = "Not Covered")) %>%
+            dplyr::mutate(pct = n / sum(n)) %>%
+            dplyr::rename(n_cov = n,
+                          pct_cov = pct)
+    } else if(spread == TRUE){
+
+        df_dist %>%
+            dplyr::summarise(distance_within = dist_indic,
+                             n_cov = sum(is_covered),
+                             pct_cov = (sum(is_covered) / nrow(.)),
+                             n_not_cov =  (sum(is_covered == 0)),
+                             pct_not_cov = (sum(is_covered == 0) / nrow(.)))
+    }
+
+}
