12925740461c7b9621fd030a1e93e9f010027955
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-11-15 19:29:08 +1000

    more thorough explanation of "nearest" works  in facility_user_dist

--- R/facility_user_dist.R
+++ R/facility_user_dist.R
@@ -2,12 +2,12 @@
 #'
 #' Uses haversines formula to calculate the distance between lat/long co-ordinates of every facility and every user, returning a data_frame. You can think of "facilities" as something like mobile towers, police centres, or AED locations, and "users" as something like individual houses, crime locations, or heart attack locations. The motivating example for this function was finding the distance from Automatic Electronic Defibrillators (AEDs) to each Out of Hospital Cardiac Arrest (OHCA), where the locations for AEDs and OHCAs are in separate dataframes. Currently facifacility_user_dist makes the strict assumption that the facility and user dataframes have columns named aed_id, lat, and long, and ohca_id, lat, and long. This will be updated soon.
 #'
-#' @param facility a dataframe containing columns aed_id, lat, and long
-#' @param user a dataframe containing columns ohca_id, lat, and long
+#' @param facility a dataframe containing columns named "lat", and "long".
+#' @param user a dataframe containing columns "lat", and "long".
 #' @param coverage_distance numeric indicating the coverage level for the facilities to be within in metres to a user. Default value is 100 metres.
-#' @param nearest character When "facility", it returns the dataframe with the nearest facilities to each user. When "user", it returns the dataframe with the nearest users to each facility. when "both" it returns the complete pairwise distances
+#' @param nearest character Can be "facility", "user", and "both". Defaults to "facility". When set to "facility", returns a dataframe where every row is every crime, and the closest building to each crime. When set to "user", returns a dataframe where every row is every building, and the closest crime to each building. set to "both", which will return every pairwise combination of distances. Be careful whenDefault is "facility"
 #'
-#' @return a data frame containing the distance between each aed and each ohca, with columns named aed_id, lat_aed, long_aed, ohca_id, lat_ohca, long_ohca, and distance - the distance in meters between each aed and ohca in a row.
+#' @return a data frame containing the two datasets joined together with columns named facility_id, lat_facility, long_facility, user_id, lat_user, long_user, distance in meters between each the given facility and user in a row.
 #'
 #' @export
 #'
@@ -62,9 +62,12 @@ facility_user_dist <- function(facility,
             dist_df %>%
             arrange(distance) %>%
             group_by(user_id) %>%
+            # find those that are closest to each other
             mutate(rank_distance = 1:n()) %>%
             ungroup() %>%
             filter(rank_distance == 1) %>%
+            # drop the rank_distance
+            select(-rank_distance) %>%
             mutate(is_covered = (distance < coverage_distance))
 
         return(dist_df)
@@ -77,6 +80,7 @@ facility_user_dist <- function(facility,
             mutate(rank_distance = 1:n()) %>%
             ungroup() %>%
             filter(rank_distance == 1) %>%
+            select(-rank_distance) %>%
             mutate(is_covered = (distance < coverage_distance))
 
         return(dist_df)
