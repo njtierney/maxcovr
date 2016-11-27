209d00c1ab00aefad15601a75f8f129b8508deea
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-11-27 23:02:34 +1000

    removed mentions of old data in  results extraction
    now works on any kind of data

--- R/extract_mc_result.R
+++ R/extract_mc_result.R
@@ -21,6 +21,8 @@ extract_mc_results <- function(x){
 
     facility_id <- readr::parse_number(colnames(x$A))
 
+    user_id <- x$user_id
+
     # which facilities are selected?
     facility_temp <- tibble::tibble(
         # get the facility ids
@@ -30,7 +32,9 @@ extract_mc_results <- function(x){
 
     facility_selected <- x$facility %>%
         mutate(facility_id = facility_id) %>%
-        dplyr::filter(facility_id %in% facility_temp$facility_id)
+        dplyr::filter(facility_id %in% facility_temp$facility_id) %>%
+        # drop facility_id as it is not needed anymore
+        select(-facility_id)
 
     # which OHCAs are affected
     user_solution <- x$lp_solution$solution[c(I+1):c(I+J)]
@@ -41,14 +45,22 @@ extract_mc_results <- function(x){
         dplyr::filter(user_chosen == 1)
 
     user_affected <- x$user %>%
-        dplyr::filter(event_id %in% user_temp$user_id)
+        mutate(user_id = user_id) %>%
+        dplyr::filter(user_id %in% user_temp$user_id) %>%
+        # drop user_id, as it not needed anymore
+        select(-user_id)
+        # dplyr::filter(event_id %in% user_temp$user_id)
 
     return(
         list(
-            facility_solution = facility_solution,
             facility_selected = facility_selected,
-            user_solution = user_solution,
-            user_affected = user_affected
+            user_affected = user_affected,
+            n_added = x$n_added
+            # not really sure if I need to provide the user + facility solution
+            # but perhaps I could provide this in another function to extract
+            # the working parts of the optimisation
+            # user_solution = user_solution,
+            # facility_solution = facility_solution,
             # facilities_users_merge = facilities_users_merge,
             #add the variables that were used here to get more info
         )
