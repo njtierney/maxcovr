8d7ef44d40e122ce011696a521e32b9044814b03
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-12-03 14:27:39 +1000

    set _R_CHECK_FORCE_SUGGESTS_=FALSE, addressing issue #22
    Thanks to @jimhester for the solution!

--- R/maxcovr-package.R
+++ R/maxcovr-package.R
@@ -7,6 +7,5 @@
 #' @useDynLib maxcovr
 #' @importFrom Rcpp sourceCpp
 NULL
-# options(R_CHECK_FORCE_SUGGESTS_=FALSE)
-Sys.setenv(_R_CHECK_FORCE_SUGGESTS_=FALSE)
+
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
