00250f915a14cc67c2e73549d966492af0fc83c2
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-01-17 17:03:53 +1000

    add tests for relocation method

--- tests/testthat/test_max_coverage.R
+++ tests/testthat/test_max_coverage.R
@@ -23,7 +23,38 @@ testthat::test_that("maximum coverage returns the correct names",{
                                         "existing_coverage",
                                         "summary",
                                         "n_added",
-                                        "distance_cutoff"))
+                                        "distance_cutoff",
+                                        "model_call"))
 
 })
+testthat::test_that("max_coverage returns has the right class",{
+    testthat::expect_is(mc_result, "maxcovr")
+    testthat::expect_true(is.maxcovr(mc_result))
+})
 
+
+mc_relocate <-  max_coverage_relocation(existing_facility = york_selected,
+                                        proposed_facility = york_unselected,
+                                        user = york_crime,
+                                        distance_cutoff = 100,
+                                        cost_install = 5000,
+                                        cost_relocate = 200,
+                                        cost_total = 600000)
+
+
+testthat::test_that("max_coverage_relocation returns the correct names",{
+    testthat::expect_named(mc_relocate, c("facility_selected",
+                                        "user_affected",
+                                        "which_existing_removed",
+                                        "model_coverage",
+                                        "existing_coverage",
+                                        "summary",
+                                        "total_cost",
+                                        "distance_cutoff",
+                                        "model_call"))
+
+})
+testthat::test_that("maximum_coverage_relocation has the right class",{
+    testthat::expect_is(mc_relocate, "maxcovr_relocation")
+    testthat::expect_true(is.maxcovr_relocation(mc_relocate))
+})
