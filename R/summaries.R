d55f0dec6f7de8a5314cee0c9da1c0db9f005ec0
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-01-24 13:43:25 +1000

    add summary_mc_cv_relocate method

--- R/summaries.R
+++ R/summaries.R
@@ -47,11 +47,11 @@ summarise_coverage <- function(df_dist,
 #' york_selected <- york %>% filter(grade == "I")
 #' york_unselected <- york %>% filter(grade != "I")
 #'
-#' mc_cv <- modelr::crossv_kfold(york_crime, 5) %>%
+#' mc_cv_fixed <- modelr::crossv_kfold(york_crime, 5) %>%
 #'                  mutate(test = map(test,as_tibble),
 #'                  train = map(train,as_tibble))
 #'
-#' mc_cv_fit <- map_df(mc_cv$train,
+#' mc_cv_fit <- map_df(mc_cv_fixed$train,
 #'                     ~max_coverage(existing_facility = york_selected,
 #'                     proposed_facility = york_unselected,
 #'                     user = .,
@@ -59,7 +59,7 @@ summarise_coverage <- function(df_dist,
 #'                     distance_cutoff = 100))
 #'
 #'  summary_mc_cv(mc_cv_fit,
-#'                mc_cv$test)
+#'                mc_cv_fixed$test)
 #'
 #' }
 #'
@@ -139,3 +139,80 @@ coverage <- function(nearest_df,
         summarise_coverage()
 
 }
+#' Summary for max_coverage cross validation for relocation models
+#'
+#' @param model the cross validated model
+#' @param test_data the cross validated test data
+#'
+#' @return a summary dataframe
+#'
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' library(maxcovr)
+#' library(tidyverse)
+#'
+#' york_selected <- york %>% filter(grade == "I")
+#' york_unselected <- york %>% filter(grade != "I")
+#'
+#' mc_cv <- modelr::crossv_kfold(york_crime, 5) %>%
+#'                  mutate(test = map(test,as_tibble),
+#'                  train = map(train,as_tibble))
+#'
+#' mc_cv_relocate <- map_df(mc_cv$train,
+#'                     ~max_coverage_relocation(existing_facility = york_selected,
+#'                                proposed_facility = york_unselected,
+#'                                user = .,
+#'                                cost_install = 2500,
+#'                                cost_removal = 700,
+#'                                cost_total = 50000,
+#'                                distance_cutoff = 100,
+#'                                solver = "gurobi"))
+#' #'
+#'  summary_mc_cv_relocate(mc_cv_relocate, mc_cv$test)
+#'
+#' }
+#'
+#' @export
+#'
+#'
+summary_mc_cv_relocate <- function(model,
+                                   test_set){
+
+        # model = mc_cv_relocate
+        # test_data = mc_cv$test
+        cost = model$total_cost[[1]]
+        purrr::pmap_df(.l = list(
+            facility_selected = model$facility_selected,
+            test_data = test_set$test,
+            dist_cutoff = model$distance_cutoff,
+            # cost = model$total_cost,
+            n_fold = test_set$.id
+        ),
+
+        .f = function(facility_selected, # the facility selected by max_coverage
+                      test_data, # the test data created by modelr
+                      dist_cutoff, # the distance cutoff
+                      n_added, # the number of AEDs added
+                      n_fold){
+            maxcovr::nearest(nearest_df = facility_selected,
+                             to_df = test_data) %>%
+                dplyr::mutate(is_covered = (distance <= dist_cutoff)) %>%
+                dplyr::summarise(
+                    # cost = cost,
+                    n_fold = n_fold,
+                    distance_within = dist_cutoff,
+                    n_cov = sum(is_covered),
+                    pct_cov = (sum(is_covered) / nrow(.)),
+                    n_not_cov =  (sum(is_covered == 0)),
+                    pct_not_cov = (sum(is_covered == 0) / nrow(.)),
+                    dist_avg = mean(distance),
+                    dist_sd = stats::sd(distance))
+
+        } # end internal function
+        ) %>%
+            dplyr::mutate(cost = cost)
+
+    } # close function
