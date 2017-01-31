b5886bf07e94029249d8b049a98d54df9c0157a3
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-01-31 16:31:43 +1000

    Add extraction functions to formalise model results extraction

--- R/extract_mc_result.R
+++ R/extract_mc_result.R
@@ -10,123 +10,190 @@
 
 extract_mc_results <- function(x){
 
-    # extract results ---------------------------------------------------------
+# find the facilities selected ------------------------------------------------
 
-    # get the dimenions from the matrix
-    J <- nrow(x$A)
-    I <- ncol(x$A)
+    mc_facilities_selected <- extract_facility_selected(
+        solution_vector = x$lp_solution$solution,
+        A_mat = x$A,
+        proposed_facilities = x$proposed_facility)
 
-    # which AEDs are to be used
-    facility_solution <- x$lp_solution$solution[1:I]
+# user_affected ---------------------------------------------------------------
 
-    facility_id <- readr::parse_number(colnames(x$A))
-
-    # user_id <- x$user_id
-
-    # which facilities are selected?
-    facility_temp <- tibble::tibble(
-        # get the facility ids
-        facility_id = facility_id,
-        facility_chosen = facility_solution) %>%
-        dplyr::filter(facility_chosen == 1)
-
-    facility_selected <- x$proposed_facility %>%
-        dplyr::mutate(facility_id = facility_id) %>%
-        dplyr::filter(facility_id %in% facility_temp$facility_id) %>%
-        # drop facility_id as it is not needed anymore
-        dplyr::select(-facility_id)
-
-    # which OHCAs are affected
-    user_solution <- x$lp_solution$solution[c(I+1):c(I+J)]
-
-    user_temp <- tibble::tibble(
+    mc_users_affected <- extract_users_affected(
+        A_mat = x$A,
+        solution_vector = x$lp_solution$solution,
         user_id = x$user_id,
-        user_chosen = user_solution) %>%
-        dplyr::filter(user_chosen == 1)
+        users_not_covered = x$user_not_covered)
 
-    user_affected <- dplyr::left_join(user_temp,
-                               x$user_not_covered,
-                               by = "user_id")
-        # x$user %>%
-        # mutate(user_id = user_id) %>%
-        # dplyr::filter(user_id %in% user_temp$user_id) %>%
-        # # drop user_id, as it not needed anymore
-        # select(-user_id)
-        # dplyr::filter(event_id %in% user_temp$user_id)
+    # return the users entered into the
+    # augmented_users - a dataframe with the original users, but with distance + a few extras added
 
-# now to return some more summaries ...
+    mc_augmented_users <- augment_user(
+        facilities_selected = mc_facilities_selected,
+        existing_facilities = x$existing_facility,
+        existing_users = x$existing_user)
 
-# NOTE: I really should use `nearest`
-    facility_sum_prep <- dplyr::bind_rows(facility_selected,
-                                          x$existing_facility) %>%
-        dplyr::select(lat,long) %>%
-        as.matrix()
+    # model_coverage
 
-    user_sum_prep <- x$existing_user %>%
-        dplyr::select(lat,long) %>%
-        as.matrix()
+    mc_model_coverage  <- extract_model_coverage(
+        augmented_user = mc_augmented_users,
+        distance_cutoff = x$distance_cutoff,
+        n_added = x$n_added)
 
-    dist_sum_df <-
-        maxcovr::nearest_facility_dist(facility = facility_sum_prep,
-                                       user = user_sum_prep) %>%
-        dplyr::as_data_frame() %>%
-        dplyr::rename(user_id = V1,
-               facility_id = V2,
-               distance = V3) %>%
-        dplyr::mutate(is_covered = (distance <= x$distance_cutoff))
+    # existing_coverage
 
-    model_coverage <- dist_sum_df %>%
-        dplyr::summarise(n_added = as.numeric(x$n_added),
-                         distance_within = as.numeric(x$distance_cutoff),
-                         n_cov = sum(is_covered),
-                         pct_cov = (sum(is_covered) / nrow(.)),
-                         n_not_cov =  (sum(is_covered == 0)),
-                         pct_not_cov = (sum(is_covered == 0) / nrow(.)),
-                         dist_avg = mean(distance),
-                         dist_sd = stats::sd(distance))
+    mc_existing_coverage <- extract_existing_coverage(
+        existing_facilities = x$existing_facility,
+        existing_users = x$existing_user,
+        distance_cutoff = x$distance_cutoff)
 
-    # add the original coverage
-    existing_coverage <- x$existing_facility %>%
-        nearest(x$existing_user) %>%
-        dplyr::mutate(is_covered = (distance <= x$distance_cutoff)) %>%
-        dplyr::summarise(n_added = 0,
-                         distance_within = as.numeric(x$distance_cutoff),
-                         n_cov = sum(is_covered),
-                         pct_cov = (sum(is_covered) / nrow(.)),
-                         n_not_cov =  (sum(is_covered == 0)),
-                         pct_not_cov = (sum(is_covered == 0) / nrow(.)),
-                         dist_avg = mean(distance),
-                         dist_sd = stats::sd(distance))
+    # summary
 
-    # add a summary coverage
-    summary_coverage = dplyr::bind_rows(existing_coverage,
-                                        model_coverage)
+    # - I dont think this is necessary anymore?
 
+    mc_summary <- bind_rows(
+        mc_existing_coverage,
+        mc_model_coverage
+    )
 
-        res <- tibble::tibble(
-            facility_selected = list(facility_selected),
-            user_affected = list(user_affected),
-            model_coverage = list(model_coverage),
-            existing_coverage = list(existing_coverage),
-            summary = list(summary_coverage),
-            n_added = list(x$n_added),
-            distance_cutoff = list(x$distance_cutoff),
-            model_call = list(x$model_call)
-        )
+    # Also need to return all of the information input into the model
+    # existing_facility
+    # proposed_facility
+    # user
 
-            # not really sure if I need to provide the user + facility solution
-            # but perhaps I could provide this in another function to extract
-            # the working parts of the optimisation
-            # user_solution = user_solution,
-            # facility_solution = facility_solution,
-            # facilities_users_merge = facilities_users_merge,
-            #add the variables that were used here to get more info
+    mc_res <- tibble::tibble(
+        n_added = list(x$n_added),
+        distance_cutoff = list(x$distance_cutoff),
+        existing_facility = list(x$existing_facility),
+        proposed_facility = list(x$proposed_facility),
+        user = list(x$existing_user),
+        user_affected = list(mc_users_affected),
+        augmented_users = list(mc_augmented_users),
+        facility_selected = list(mc_facilities_selected),
+        model_coverage = list(mc_model_coverage),
+        existing_coverage = list(mc_existing_coverage),
+        summary = list(mc_summary),
+        model_call = list(x$model_call),
+        solution = list(x$lp_solution)
+    )
+#
+#     # extract results ---------------------------------------------------------
+#
+#     # get the dimenions from the matrix
+#     J <- nrow(x$A)
+#     I <- ncol(x$A)
+#
+#     # which AEDs are to be used
+#     facility_solution <- x$lp_solution$solution[1:I]
+#
+#     facility_id <- readr::parse_number(colnames(x$A))
+#
+#     # user_id <- x$user_id
+#
+#     # which facilities are selected?
+#     facility_temp <- tibble::tibble(
+#         # get the facility ids
+#         facility_id = facility_id,
+#         facility_chosen = facility_solution) %>%
+#         dplyr::filter(facility_chosen == 1)
+#
+#     facility_selected <- x$proposed_facility %>%
+#         dplyr::mutate(facility_id = facility_id) %>%
+#         dplyr::filter(facility_id %in% facility_temp$facility_id) %>%
+#         # drop facility_id as it is not needed anymore
+#         dplyr::select(-facility_id)
+#
+#     # which OHCAs are affected
+#     user_solution <- x$lp_solution$solution[c(I + 1):c(I + J)]
+#
+#     user_temp <- tibble::tibble(
+#         user_id = x$user_id,
+#         user_chosen = user_solution) %>%
+#         dplyr::filter(user_chosen == 1)
+#
+#     user_affected <- dplyr::left_join(user_temp,
+#                                x$user_not_covered,
+#                                by = "user_id")
+#         # x$user %>%
+#         # mutate(user_id = user_id) %>%
+#         # dplyr::filter(user_id %in% user_temp$user_id) %>%
+#         # # drop user_id, as it not needed anymore
+#         # select(-user_id)
+#         # dplyr::filter(event_id %in% user_temp$user_id)
+#
+# # now to return some more summaries ...
+#
+# # NOTE: I really should use `nearest`
+#     facility_sum_prep <- dplyr::bind_rows(facility_selected,
+#                                           x$existing_facility) %>%
+#         dplyr::select(lat,long) %>%
+#         as.matrix()
+#
+#     user_sum_prep <- x$existing_user %>%
+#         dplyr::select(lat,long) %>%
+#         as.matrix()
+#
+#     dist_sum_df <-
+#         maxcovr::nearest_facility_dist(facility = facility_sum_prep,
+#                                        user = user_sum_prep) %>%
+#         dplyr::as_data_frame() %>%
+#         dplyr::rename(user_id = V1,
+#                facility_id = V2,
+#                distance = V3) %>%
+#         dplyr::mutate(is_covered = (distance <= x$distance_cutoff))
+#
+#     model_coverage <- dist_sum_df %>%
+#         dplyr::summarise(n_added = as.numeric(x$n_added),
+#                          distance_within = as.numeric(x$distance_cutoff),
+#                          n_cov = sum(is_covered),
+#                          pct_cov = mean(is_covered),
+#                          n_not_cov =  (sum(is_covered == 0)),
+#                          pct_not_cov = 1 - mean(is_covered),
+#                          dist_avg = mean(distance),
+#                          dist_sd = stats::sd(distance))
+#
+#     # add the original coverage
+#     existing_coverage <- x$existing_facility %>%
+#         nearest(x$existing_user) %>%
+#         dplyr::mutate(is_covered = (distance <= x$distance_cutoff)) %>%
+#         dplyr::summarise(n_added = 0,
+#                          distance_within = as.numeric(x$distance_cutoff),
+#                          n_cov = sum(is_covered),
+#                          pct_cov = (sum(is_covered) / nrow(.)),
+#                          n_not_cov =  (sum(is_covered == 0)),
+#                          pct_not_cov = (sum(is_covered == 0) / nrow(.)),
+#                          dist_avg = mean(distance),
+#                          dist_sd = stats::sd(distance))
+#
+#     # add a summary coverage
+#     summary_coverage = dplyr::bind_rows(existing_coverage,
+#                                         model_coverage)
+#
+#
+#         res <- tibble::tibble(
+#             facility_selected = list(facility_selected),
+#             user_affected = list(user_affected),
+#             model_coverage = list(model_coverage),
+#             existing_coverage = list(existing_coverage),
+#             summary = list(summary_coverage),
+#             n_added = list(x$n_added),
+#             distance_cutoff = list(x$distance_cutoff),
+#             model_call = list(x$model_call)
+#         )
+#
+#             # not really sure if I need to provide the user + facility solution
+#             # but perhaps I could provide this in another function to extract
+#             # the working parts of the optimisation
+#             # user_solution = user_solution,
+#             # facility_solution = facility_solution,
+#             # facilities_users_merge = facilities_users_merge,
+#             #add the variables that were used here to get more info
 
     # res <- c(class(res),"maxcovr_relocation")
-    class(res) <- c("maxcovr",class(res))
+    class(mc_res) <- c("maxcovr",class(mc_res))
     # class(res) <- c("maxcovr_relocation")
 
-    return(res)
+    return(mc_res)
 
 }
 
---
+++ R/extractors.R
@@ -0,0 +1,238 @@
+# These are functions that extract results from the lp_solve in maxcovr
+# fixed locations
+
+
+#' Extract Selected Facilities
+#'
+#' This takes the linear programming solution, the A matrix, and the proposed
+#' facilities. It returns a tibble, which contains the facilities chosen from
+#' the proposed facilities.
+#'
+#' @param solution_vector vector from lp_solution$solution
+#' @param A_mat The "A" matrix from the solver
+#' @param proposed_facilities Dataframe of proposed facilities
+#'
+#' @return dataframe of selected facilities
+#'
+#' @examples
+#'
+#' # assuming that you've run max_coverage using lpSolve, then you
+#' # will save the model output before the extraction process
+#' # as `x`.
+#' \dontrun{
+#' mc_facilities_selected <- extract_facility_selected(
+#'   solution_vector = x$lp_solution$solution,
+#'   A_mat = x$A,
+#'   proposed_facilities = x$proposed_facility)
+#'   }
+#'
+extract_facility_selected <- function(solution_vector,
+                                      A_mat,
+                                      proposed_facilities){
+
+    # the number of facilities, is given by the number of columns in A.
+    # Ideally, this should be related to the number of facilities
+    # like ncol(proposed_facilities)
+
+    I <- ncol(A_mat)
+
+    facility_solution <- solution_vector[1:I]
+
+    facility_id <- readr::parse_number(colnames(A_mat))
+
+    # which facilities are selected?
+    facility_temp <- tibble::tibble(
+        # get the facility ids
+        facility_id = facility_id,
+        facility_chosen = facility_solution) %>%
+        dplyr::filter(facility_chosen == 1)
+
+    # join these back on.
+    facility_selected <- proposed_facilities %>%
+        dplyr::mutate(facility_id = facility_id) %>%
+        dplyr::filter(facility_id %in% facility_temp$facility_id) %>%
+        # drop facility_id as it is not needed anymore
+        dplyr::select(-facility_id)
+
+    return(facility_selected)
+
+}
+
+#' Extract users affected
+#'
+#' Extract additional users affected by new coverage from the new facilities
+#'
+#' @param A_mat A matrix
+#' @param solution_vector The vector of solutions
+#' @param user_id The IDs of the individuals
+#' @param users_not_covered # those users not covered by original AEDs
+#'
+#' @return tibble taken from `users`, those who are affectd by new placements
+#'
+#' @examples
+#'
+#' \dontrun{
+#' extract_users_affected(
+#'     A_mat = x$A,
+#'     solution_vector = x$lp_solution$solution,
+#'     user_id = x$user_id,
+#'     users_not_covered = x$user_not_covered)
+#'     }
+#'
+extract_users_affected <- function(A_mat,
+                                   solution_vector,
+                                   user_id,
+                                   users_not_covered){
+
+    I <- ncol(A_mat)
+    J <- nrow(A_mat)
+
+    user_solution <- solution_vector[c(I + 1):c(I + J)]
+
+    user_temp <- tibble::tibble(
+        user_id = user_id,
+        user_chosen = user_solution) %>%
+        dplyr::filter(user_chosen == 1)
+
+    user_affected <- dplyr::left_join(user_temp,
+                                      users_not_covered,
+                                      by = "user_id")
+
+    return(user_affected)
+
+}
+
+#' Augment the users data; add useful information
+#'
+#' This returns the `user` dataframe, with added columns containing distance
+#' between that user and a given facility - IDs are generated for IDs and
+#' facilities that correspond to their row number.
+#'
+#' @param facilities_selected dataframe of facilities selected, obtained from \code{extract_facility_selected}
+#' @param existing_facilities existing facilities
+#' @param existing_users existing users
+#'
+#' @return tibble of users, with distances between each user and facility
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' mc_facilities_selected <-extract_facility_selected(
+#'   solution_vector = x$lp_solution$solution,
+#'   A_mat = x$A,
+#'   proposed_facilities = x$proposed_facility)
+#'
+#' augmented_users <- augment_user(
+#'     facilities_selected = mc_facilities_selected,
+#'     existing_facilities = mc_cv_fit_n20_test_1$existing_facility,
+#'     existing_users = mc_cv_fit_n20_test_1$existing_user
+#'     )
+#' }
+#'
+augment_user <- function(facilities_selected,
+                         existing_facilities,
+                         existing_users){
+
+    # bind the selected and existing facilities together
+    all_facilities <- dplyr::bind_rows({
+        facilities_selected %>%
+            dplyr::select(lat,long) %>%
+            dplyr::mutate(type = "selected")
+    },{
+        existing_facilities %>%
+            dplyr::select(lat,long) %>%
+            dplyr::mutate(type = "existing")
+    })
+
+    # now return the distances etc
+    augmented_users <- nearest(all_facilities,
+                               existing_users)
+
+    return(augmented_users)
+
+}
+
+#' Extract a one-row summary of the model coverage
+#'
+#' This function takes the users information, the distance cutoff, and the number of facilities added, and then returns a one-row dataframe containing summary information about the coverage.
+#'
+#' @param augmented_user dataframe obtained from augment_user()
+#' @param distance_cutoff numeric of the distance cutoff
+#' @param n_added numeric of the number of facilities added
+#'
+#' @return tibble of summary coverage info
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' augmented_users <- augment_user(
+#'     facilities_selected = mc_facilities_selected,
+#'     existing_facilities = x$existing_facility,
+#'     existing_users = x$existing_user)
+#'
+#' extract_model_coverage(
+#'     augmented_user = augmented_users,
+#'     distance_cutoff = x$distance_cutoff,
+#'     n_added = x$n_added)
+#' }
+#'
+
+extract_model_coverage <- function(augmented_user,
+                                   distance_cutoff,
+                                   n_added){
+
+    augmented_user %>%
+        dplyr::mutate(is_covered = (distance <= distance_cutoff)) %>%
+        dplyr::summarise(n_added = as.numeric(n_added),
+                         distance_within = as.numeric(distance_cutoff),
+                         n_cov = sum(is_covered),
+                         pct_cov = mean(is_covered),
+                         n_not_cov =  (sum(is_covered == 0)),
+                         pct_not_cov = 1 - mean(is_covered),
+                         dist_avg = mean(distance),
+                         dist_sd = stats::sd(distance))
+
+}
+
+#' Extract the existing coverage
+#'
+#' @param existing_facilities the existing facilities
+#' @param existing_users the existing users
+#' @param distance_cutoff the distance cutoffs
+#'
+#' @return tibble of existing coverage
+#'
+#' @examples
+#'
+#' \dontrun{
+#' extract_existing_coverage(existing_facilities = x$existing_facility,
+#'    existing_users = x$existing_user,
+#'    distance_cutoff = x$distance_cutoff)
+#'    }
+
+extract_existing_coverage <- function(existing_facilities,
+                                      existing_users,
+                                      distance_cutoff){
+
+    existing_coverage <- nearest(existing_facilities,
+                                 existing_users) %>%
+        dplyr::mutate(is_covered = (distance <= distance_cutoff)) %>%
+        # very similar summary function is called here and above,
+        # ideally this should be written up as a function
+          # it is already written up as `summarise_coverage`
+          # but there needs to be a better way to do this...
+        dplyr::summarise(n_added = 0,
+                         distance_within = as.numeric(distance_cutoff),
+                         n_cov = sum(is_covered),
+                         pct_cov = mean(is_covered),
+                         n_not_cov =  (sum(is_covered == 0)),
+                         pct_not_cov = 1 - mean(is_covered),
+                         dist_avg = mean(distance),
+                         dist_sd = stats::sd(distance))
+
+
+    return(existing_coverage)
+}
+
