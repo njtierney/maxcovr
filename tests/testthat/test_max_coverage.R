d15fca04af5bf25b0dae05d06378fdf04d239fe9
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-07-05 13:42:52 +1000

    split max_coverage_relocation into separate test

--- tests/testthat/test_max_coverage.R
+++ tests/testthat/test_max_coverage.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tibble)
 library(tidyr)
 
-context("max_coverage works")
+context("max_coverage")
 
 york_selected <- york %>% dplyr::filter(grade == "I")
 york_unselected <- york %>% dplyr::filter(grade != "I")
@@ -39,31 +39,3 @@ testthat::test_that("max_coverage returns has the right class",{
     testthat::expect_is(mc_result, "maxcovr")
     testthat::expect_true(is.maxcovr(mc_result))
     })
-
-
-mc_relocate <-  max_coverage_relocation(existing_facility = york_selected,
-                                        proposed_facility = york_unselected,
-                                        user = york_crime,
-                                        distance_cutoff = 100,
-                                        cost_install = 500,
-                                        cost_removal = 100,
-                                        cost_total = 1000)
-
-# mc_relocate
-
-testthat::test_that("max_coverage_relocation returns the correct names",{
-    testthat::expect_named(mc_relocate, c("facility_selected",
-                                          "user_affected",
-                                          "which_existing_removed",
-                                          "model_coverage",
-                                          "existing_coverage",
-                                          "summary",
-                                          "total_cost",
-                                          "distance_cutoff",
-                                          "model_call"))
-
-})
-testthat::test_that("maximum_coverage_relocation has the right class",{
-    testthat::expect_is(mc_relocate, "maxcovr_relocation")
-    testthat::expect_true(is.maxcovr_relocation(mc_relocate))
-})
