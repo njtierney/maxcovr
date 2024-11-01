dffbe60dfdcf408750bbe862a7713031dad506f1
rafapereirabr <rafa.pereira.br@gmail.com>
2024-11-01 19:30:39 -0300

    documentation

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // deg2rad_cpp
 double deg2rad_cpp(double deg);
 RcppExport SEXP _maxcovr_deg2rad_cpp(SEXP degSEXP) {
